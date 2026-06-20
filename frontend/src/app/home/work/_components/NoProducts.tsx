a185370375277a73affdda3143b61431d78fe47d
mudzinpl <mudzinpl@gmail.com>
2026-06-20 22:27:08 +0200

    feat(i18n): translate empty work state

--- frontend/src/app/home/work/_components/NoProducts.tsx
+++ frontend/src/app/home/work/_components/NoProducts.tsx
@@ -12,14 +12,14 @@ export default function NoProducts({
     const editUrl = `/home/work/${work.id}/${activityId}/edit/startfresh`;
     return ( <div className="text-center">
                             
-        <h3 className="mt-2 text-sm font-semibold text-gray-900">No products or services yet</h3> 
+        <h3 className="mt-2 text-sm font-semibold text-gray-900">Brak produktów i usług</h3> 
        {!work.issuance&& <div className="my-6">
           <Link href={editUrl}
             type="button"
             className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             <PlusIcon aria-hidden="true" className="mr-1.5 -ml-0.5 size-5" />
-            Start
+            Rozpocznij
           </Link>
         </div>}
       </div>)
