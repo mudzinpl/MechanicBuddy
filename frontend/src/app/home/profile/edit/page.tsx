dbcdbe1584ce8204c39800b31aa26437290eb97a
mudzinpl <mudzinpl@gmail.com>
2026-06-20 22:40:36 +0200

    fix(i18n): translate save button

--- frontend/src/app/home/profile/edit/page.tsx
+++ frontend/src/app/home/profile/edit/page.tsx
@@ -75,7 +75,7 @@ export default async function Page() {
                 type="submit"
                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Save
+                Zapisz
               </button>
             </div>
           </div>
