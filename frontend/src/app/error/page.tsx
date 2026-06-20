c170259b62b065864a9167f0cbb967cc94084caa
mudzinpl <mudzinpl@gmail.com>
2026-06-20 22:04:25 +0200

    feat(i18n): translate error page

--- frontend/src/app/error/page.tsx
+++ frontend/src/app/error/page.tsx
@@ -15,7 +15,7 @@ function Error(){
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">{code} {statusText}</p>
           <h1 className="mt-4 text-3xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
-            Oops, something went wrong
+            Ups, coś poszło nie tak
           </h1>
           <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
             {text}
@@ -25,10 +25,10 @@ function Error(){
               href="/home/work"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Go back home
+              Wróć do strony głównej
             </Link>
             <a href="/support" className="text-sm font-semibold text-gray-900">
-              Contact support <span aria-hidden="true">&rarr;</span>
+              Skontaktuj się z pomocą techniczną <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </div>
