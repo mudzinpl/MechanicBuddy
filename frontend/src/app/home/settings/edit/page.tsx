6729a6178b90dc4d9b84557a831ab6ae0e60a9f8
mudzinpl <mudzinpl@gmail.com>
2026-06-28 00:37:14 +0200

    Polish company settings form actions

--- frontend/src/app/home/settings/edit/page.tsx
+++ frontend/src/app/home/settings/edit/page.tsx
@@ -10,6 +10,7 @@ import FormLabel from '@/_components/FormLabel';
 import FormTextArea from '@/_components/FormTextArea';
 import FormSwitch from '@/_components/FormSwitch';
 import { createOrUpdate } from '../createOrUpdate'; 
+import Link from 'next/link';
 
  
 
@@ -30,7 +31,7 @@ export default async function Page( ) {
           <h2 className="text-base/7 font-semibold text-gray-900  my-4">Dane firmy</h2>
           
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-8 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
             <div className="sm:col-span-4">
                 <FormInput name='name' label='Nazwa'  defaultValue={options.requisites.name}></FormInput> 
               </div>
@@ -58,7 +59,7 @@ export default async function Page( ) {
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base/7 font-semibold text-gray-900">Ustawienia faktury</h2> 
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-8 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
              <div className="sm:col-span-2">
                 <FormInput name='vatRate' label='Stawka VAT' defaultValue={options.pricing.invoice.vatRate}></FormInput> 
               </div>
@@ -83,7 +84,7 @@ export default async function Page( ) {
 
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base/7 font-semibold text-gray-900">Ustawienia oferty</h2> 
-          <div className="mt-10 space-y-10">
+          <div className="mt-8 space-y-8">
           <div className="sm:col-span-full">
                 <FormTextArea name='estimateEmailContent' rows={10} label='Treść wiadomości e-mail' defaultValue={options.pricing.estimate.emailContent}></FormTextArea> 
               </div> 
@@ -92,12 +93,17 @@ export default async function Page( ) {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        
+        <Link
+          href="/home/settings"
+          className="text-sm font-semibold text-gray-900"
+        >
+          Anuluj
+        </Link>
         <button
           type="submit"
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Save
+          Zapisz
         </button>
       </div>
     </form>
